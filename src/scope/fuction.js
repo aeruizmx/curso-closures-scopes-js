d9e7737043a884408f579f49fa45f5b98ac8cd6c
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-07-29 11:42:59 -0500

    Clase 4

---
+++ src/scope/fuction.js
@@ -0,0 +1,17 @@
+const fruits = () => {
+  var fruit = 'apple';
+  console.log(fruit);
+};
+fruits();
+//console.log(fruit); // ReferenceError: fruit is not defined
+
+const anotherFunction = () =>{
+  var x = 1;
+  var x = 2;
+  let y = 1;
+  //let y = 2; //SyntaxError: Identifier 'y' has already been declared
+  console.log(x);
+  console.log(y);
+}
+
+anotherFunction();
